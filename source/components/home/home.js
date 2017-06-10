a3dbb08bcc2f11fede9143a43afe341b11ed8a5d
Simon Lu <lusimon95@gmail.com>
2017-06-10 14:27:38 -0500

    hackillinois initial commit; stack setup done~

---
+++ source/components/home/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import styles from './home.scss'
+
+class Home extends Component {
+    render() {
+        return(
+            <div>Home</div>
+        )
+    }
+}
+
+export default Home
