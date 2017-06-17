74e0dbab46e3352e2787d233e40c38e554650913
Simon Lu <lusimon95@gmail.com>
2017-06-16 20:49:38 -0500

    initial commit; small bug to fix on sunday still :')

--- source/components/home/home.js
+++ source/components/home/home.js
@@ -5,7 +5,12 @@ import styles from './home.scss'
 class Home extends Component {
     render() {
         return(
-            <div>Home</div>
+            <div className="Landing">
+              <img className="Landing__logo" src="./assets/img/png/landing_page_logo.png" />
+              <video playsinline autoplay muted loop poster="" className="Landing__background">
+                <source src="./assets/video/landing_page_bg.mp4" type="video/mp4" />
+              </video>
+            </div>
         )
     }
 }
