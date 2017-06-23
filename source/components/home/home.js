2440db3d57743f5de9298938b427cf83eff066b9
Simon Lu <lusimon95@gmail.com>
2017-06-22 20:49:06 -0500

    added fallback video containers (.webm) in case browsers don't support mp4

--- source/components/home/home.js
+++ source/components/home/home.js
@@ -7,8 +7,9 @@ class Home extends Component {
         return(
             <div className="Landing">
               <img className="Landing__logo" src="./assets/img/png/landing_page_logo.png" />
-              <video playsinline autoplay muted loop poster="" className="Landing__background">
+              <video autoPlay loop id="video-background" className="Landing__background" muted playsInline>
                 <source src="./assets/video/landing_page_bg.mp4" type="video/mp4" />
+                <source src="./assets/video/landing_page_bg.webm" type="video/webm" />
               </video>
             </div>
         )
