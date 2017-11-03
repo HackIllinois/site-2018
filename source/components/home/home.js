dd668411c808946f1e9737a1cc9111a0cefb74aa
Yash Sharma <yashosharma@gmail.com>
2017-11-02 21:34:21 -0500

    Revert "About and FAQ sections"

--- source/components/home/home.js
+++ source/components/home/home.js
@@ -2,28 +2,13 @@ import React, { Component } from 'react'
 
 import styles from './home.scss'
 
-import About from '../about/about.jsx'
-import FAQ from '../faq/faq.jsx'
-import SectionHeader from '../sectionHeader/sectionHeader.jsx'
-
 class Home extends Component {
     render() {
         return(
-            <div className="container">
-                <div className="Background">
-                    <div className="Background__footer">
-
-                    </div>
-                </div>
-
-                <div className="Landing">
-                  <img className="Landing__logo" src="./assets/img/png/landing_page_logo_date.png" />
-                </div>
-
-                <About/>
-
-                <SectionHeader text="Frequently Asked Questions" />
-                <FAQ/>
+            <div className="Landing">
+              <img className="Landing__logo" src="./assets/img/png/landing_page_logo_date.png" />
+              <div className="Landing__background">
+              </div>
             </div>
         )
     }
