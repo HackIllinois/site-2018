c752f7ea5f98b57160d43a5cd2f6d7763a997065
Yash Sharma <yashosharma@gmail.com>
2017-11-16 14:47:18 -0600

    Removed redundant env vars

--- scripts/invalidate.sh
+++ scripts/invalidate.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-AWS_ACCESS_KEY_ID=$HACKILLINOIS_ACCESS_KEY_ID
-AWS_SECRET_ACCESS_KEY=$HACKILLINOIS_SECRET_ACCESS_KEY
-
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION --paths '/*'
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_IDENTIFIER/purge_cache" \
   -H "X-Auth-Email: $CLOUDFLARE_AUTH_EMAIL" \
