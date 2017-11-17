b46d67f60cef6e8b35dabbf245c0ecfbde28e524
Yash Sharma <yashosharma@gmail.com>
2017-11-17 08:55:00 -0600

    Update auth.js

--- source/components/auth/auth.js
+++ source/components/auth/auth.js
@@ -11,7 +11,7 @@ class Auth extends Component {
 
     componentDidMount() {
       const code = querystring.parse(this.props.location.search)['code']
-      const token = fromPromise(axios.get('http://ec2-107-20-14-41.compute-1.amazonaws.com/v1/auth/github?code=' + code))
+      const token = fromPromise(axios.get('https://api.hackillinois.org/v1/auth/github?code=' + code))
       .then(
         (token) => {
           sessionStorage.setItem("Authorization", 'Bearer ' + token.data.data.auth);
