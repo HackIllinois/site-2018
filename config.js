ace2550c3d1c5b719b6d2e97c9dd4362d984a1bf
Yash Sharma <yashosharma@gmail.com>
2018-01-23 19:01:19 -0600

    Set prod api url based on travis deploy

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-const isProd = false;
+const isProd = process.env["TRAVIS_BRANCH"] !== undefined && process.env["TRAVIS_BRANCH"] === "master";
 
 const api_url = isProd ? "https://api.hackillinois.org" : "http://api.test.hackillinois.org"
 
