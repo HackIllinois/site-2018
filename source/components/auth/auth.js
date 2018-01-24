355d80c9d2c153ba89c335a4a9c21ba5c7f2be64
Yasha Mostofi <yasha.mostofi@gmail.com>
2018-01-23 20:17:51 -0600

    Revert "Config file for api"

--- source/components/auth/auth.js
+++ source/components/auth/auth.js
@@ -3,8 +3,6 @@ import querystring from 'query-string';
 
 import axios from 'axios';
 
-import {api_url} from '../../../config';
-
 class Auth extends Component {
   constructor(props) {
       super(props);
@@ -13,7 +11,7 @@ class Auth extends Component {
 
   componentDidMount() {
     const code = querystring.parse(this.props.location.search)['code'];
-    axios.get(api_url + '/v1/auth/github?code=' + code)
+    axios.get('https://api.hackillinois.org/v1/auth/github?code=' + code)
     .then(
       (token) => {
         sessionStorage.setItem("Authorization", 'Bearer ' + token.data.data.auth);
