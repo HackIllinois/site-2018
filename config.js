d209ae8ebabdec65281a7c6314848df7bb56d36a
Yash Sharma <yashosharma@gmail.com>
2018-01-24 21:35:14 -0600

    wpack injection

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-
-const isProd = window.location.hostname === "hackillinois.org" || window.location.hostname === "www.hackillinois.org"
-
-const api_url = isProd ? "https://api.hackillinois.org" : "http://api.test.hackillinois.org"
+const api_url = HACKILLINOIS_API_ENDPOINT
 
 export {api_url};
