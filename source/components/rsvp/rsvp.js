ae9ef6d8bd798cd27eea4bb8bf5df5fcb022b218
Yasha Mostofi <yasha.mostofi@gmail.com>
2018-02-08 10:10:29 -0600

    Remove GA

--- source/components/rsvp/rsvp.js
+++ source/components/rsvp/rsvp.js
@@ -4,15 +4,6 @@ import { Grid, Form, Button, Image} from 'semantic-ui-react';
 import {getAuth, sendRsvp, getRSVPData} from './rsvpHelper'
 import styles from './rsvp.scss'
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-46010489-2', {
-      'cookieDomain': 'hackillinois.org'
-  });
-
-
 export default class Rsvp extends Component {
   constructor(props) {
     super(props);
